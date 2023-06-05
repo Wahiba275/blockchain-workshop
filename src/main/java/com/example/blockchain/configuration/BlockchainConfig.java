f8e61f04a644049b4d92eb1e5f8d66706aae1e29
Wahiba275 <123891320+Wahiba275@users.noreply.github.com>
2023-06-05 14:59:38 +0000

    Initial commit

---
+++ src/main/java/com/example/blockchain/configuration/BlockchainConfig.java
@@ -0,0 +1,16 @@
+package com.example.blockchain.configuration;
+
+import com.example.blockchain.entities.Blockchain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class BlockchainConfig {
+
+    @Bean
+    @Scope("singleton")
+    public Blockchain blockchain() {
+        return new Blockchain(2);
+    }
+}
